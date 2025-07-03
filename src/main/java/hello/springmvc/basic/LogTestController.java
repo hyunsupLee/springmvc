3865e63c375e93d056b4d9e62ac866f5291deff2
hyunsubLee <akmei90@gmail.com>
2025-07-03 23:32:48 +0900

    logger test

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,20 +1,27 @@
 package hello.springmvc.basic;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
-    private final Logger log = LoggerFactory.getLogger(getClass());
+//    private final Logger log = LoggerFactory.getLogger(getClass()); lombok의 @Slf4j 애노테이션을 사용하면 <-- 이코드는 필요 없다.
 
     @GetMapping("/log-test")
     public String logTet() {
         String name = "spring";
-        System.out.println("name = " + name);
-        log.info("info log={}", name);
+
+        // logging.level.hello.springmvc=info 기본셋팅은 INFO 이다.
+        // log level trace > debug > info > warn > error
+        System.out.println("name = " + name); // 상황에 따른 로그 제한 없이 다 찍히는 게 문제다.
+        log.trace("trace log={}",name);
+        log.debug("debug log={}",name);
+        log.info("info log={}",name);
+        log.warn("warn log={}",name);
+        log.error("error log={}", name);
         return "ok";
     }
 }
